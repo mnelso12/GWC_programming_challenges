75b2e3c5d431fac00b60be9de471a4fad763b8c4
Madelyn Nelson <mnelso12@nd.edu>
2017-07-29 09:53:09 -0400

    second programming challenge

---
+++ challenge2.py
@@ -0,0 +1,30 @@
+############################################
+# Programming Challenge 2
+# Python: Variables, Conditionals, Loops, & Functions
+############################################
+
+# 1
+# print "Python is a scripting language"
+
+
+
+# 2
+# print "Nishitha" 4 times using a while loop
+
+
+
+# 3
+# write a function that prints all the multiples of 5 between 2 and 31 using mod 
+
+
+
+# 4
+# write a function called "smush" that concatenates two strings and prints the result 
+
+
+
+# 5
+# write a function called "print_list" that takes in a list as a parameter, prints all of the elements in a list, and prints the length of the list 
+
+
+
